26ccde4985220f7b955cf3bef7a4598c6c8bad8e
etp1711 <etpetp1711@gmail.com>
2012-12-22 23:05:03 +0330

    add first page

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>AlborzLab</title>
+	<link rel="stylesheet" type="text/css" href="reset.css"/>
+	<link rel="stylesheet" type="text/css" href="1styles.css"/>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+<div class="content-container">
+	
+</div>
+</body>
+</html>

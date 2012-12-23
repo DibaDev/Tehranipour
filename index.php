ef8988f1762381bc6dda2e1f4569112ee4bb24cb
etp1711 <etpetp1711@gmail.com>
2012-12-23 16:02:13 +0330

    last edit

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 	<link rel="stylesheet" type="text/css" href="reset.css"/>
 	<link rel="stylesheet" type="text/css" href="1styles.css"/>
 	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<script type="text/javascript">
+		var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'];
+    var curImage = 0;
+    function switchImage()
+    {
+        curImage = (curImage + 1) % images.length
+        document.body.style.backgroundImage = 'url(images/' + images[curImage] + ')'
+    }
+    window.setInterval(switchImage, 15000);
+    
+</script>
 </head>
 
 <body>
@@ -16,10 +27,10 @@
 				<div class="logo">Ebrahim Tehranipour</div>	
 				<div class="menu">
 					<ul>
-						<li><a href="">HOME</a></li>
-						<li><a href="">ABOUT</a></li>
+						<li><a href="">EMAIL</a></li>
 						<li><a href="">GALLERY</a></li>
-						<li><a href="">MAIL</a></li>
+						<li><a href="">ABOUT</a></li>
+						<li><a href="">HOME</a></li>
 					</ul>
 				</div>
 				<div class="clear"></div>

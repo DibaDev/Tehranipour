d5d52fe5b5890ab4d515ef17f3c7bf281817aecd
etp1711 <etpetp1711@gmail.com>
2012-12-24 13:02:10 +0330

    last edition

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="reset.css"/>
 	<link rel="stylesheet" type="text/css" href="1styles.css"/>
 	<link rel="stylesheet" type="text/css" href="style.css"/>
-	<script type="text/javascript">
+	<!-- <script type="text/javascript">
 		var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'];
     var curImage = 0;
     function switchImage()
@@ -16,7 +16,7 @@
     }
     window.setInterval(switchImage, 15000);
     
-</script>
+</script> -->
 </head>
 
 <body>
@@ -39,12 +39,31 @@
 		</section>
 	</header>
 	<footer class="foot">
-		<section class="slide">
-			
+		<section class="slideshow">
+			<div class="next"></div>
+			<div class="previous"></div>
+			<div class="train">
+				<ul>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+					<li><a href=""><img src="images/1s.jpg" alt="" /></a></li>
+				</ul>
+			</div>
+
 		</section>
-		<div class="text">
-			<p>{ EBRAHIM TEHRANIPOUR } © 2012&nbsp; |&nbsp; ALL RIGHTS RESERVED</p>
-		</div>
+		<!--<div class="text">
+		<p>{ EBRAHIM TEHRANIPOUR } © 2012&nbsp; |&nbsp; ALL RIGHTS RESERVED</p>
+		</div>-->
 	</footer>	
 	
 </div>
